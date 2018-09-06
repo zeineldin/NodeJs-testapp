ce0e67e604a134e8264b40959b9f94a67698073c
zeineldin <m.zeineldin@leverzein.me>
2018-09-06 15:49:02 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+# npm install 
+# npm run lint  
+ --> testing linting error .. quality of the code 
+
+
---
+++ index.spec.js
@@ -0,0 +1,5 @@
+describe("zain el7elwo",function(){ 
+	it("should be 7elwo",function(){
+	expect("helow").toEqual("helow");
+	})
+})
